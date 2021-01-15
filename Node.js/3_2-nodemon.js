c1b5531c9067e00700620e9a812b17a5e461149f
pseeej <jinny8517@naver.com>
2021-01-16 01:13:43 +0900

    2021-01-15
    view engine, template 상속

--- Node.js/3_2-nodemon.js
+++ Node.js/3_2-nodemon.js
@@ -8,3 +8,5 @@ http.createServer(function(request, response){
 
 //nodemon 설치 안 해도 되게 하려면 npx 이용
 //npx nodemon ~~하면 됨
+
+//package.json이 npm start하려는 directory랑 같은 위치에 있어야 돌아간다는 것을 깨달음!
--- Node.js/3_4-ViewEngine.js
+++ Node.js/3_4-ViewEngine.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const admin = require('./3_3-Routes/3-3_admin');    //해당 주소에 있는 내용 가져오기
 const contacts = require('./3_3-Routes/3-3_contacts');
 
 const app = express();
 const port = 3000;
+
+nunjucks.configure('Template', {
+    autoescape : true,  //웬만한 html의 템플릿 보안 향상을 위해. 크로스사이트 공격같은 거,,,?
+    express : app
+});
 
 app.get('/', (req, res) => {
     res.send('express start');
