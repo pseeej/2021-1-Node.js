732d9207423c0720eb5cfba1f11189a99c9d86ec
pseeej <jinny8517@naver.com>
2021-01-18 12:33:26 +0900

    DB 조회 (2021-01-18)
    작성한 내용 전체 글 목록에 올리기

--- Node.js/4. Sequelize/controllers/admin/admin.ctrl.js
+++ Node.js/4. Sequelize/controllers/admin/admin.ctrl.js
@@ -1,9 +1,16 @@
 const models = require('../../models');
 
 exports.get_products = ( _ , res) => {
-    res.render( 'admin/products.html' , 
-        { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
-    );
+    // res.render( 'admin/products.html' , 
+    //     { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
+    // );
+
+    models.Products.findAll({   //전체 조회하기
+
+    }).then( (products) => {
+        res.render('admin/products.html' , { products : products })  //template로 보내기. products:products같이 key값이랑 value값 일치하면 하나만 써도 됨
+        // nunjucks에서 반복문 뿌리기 위해 products.html로 이동해서 {% for product in productList %} ~ {% endfor %} 입력해줌
+    });
 }
 
 exports.get_products_write = ( _ , res) => {
@@ -20,5 +27,5 @@ exports.post_products_write = ( req , res ) => {
         //}
     ).then(()=>{
         res.redirect('/admin/products');    //다 하고 나서 main page로 이동해서 list를 보고 싶다. redirect 사용.
-    })
+    });
 }   //다 하고 workbench 들어가서 products 확인하면 입력된 거 들어가있음
