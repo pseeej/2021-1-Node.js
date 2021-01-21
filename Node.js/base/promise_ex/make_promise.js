9ad6befd5c2d974f973dd54752a02ef7d3d38508
pseeej <jinny8517@naver.com>
2021-01-22 00:11:07 +0900

    promise란
    지연 응답을 통해서 동시성을 제어하기 위함... resolve로 내가 원하는 시점 받아오고 reject로 error 처리함

---
+++ Node.js/base/promise_ex/make_promise.js
@@ -0,0 +1,20 @@
+// Promise : 지연 응답을 통해서 동시성을 제어 하기 위함
+// Promise 만드는 규칙
+// resolve와 reject를 인자로 가지는 함수 만들기
+// 내가 원하는 시점을 resolve로 받아오기
+// reject는 내가 error 처리하는 거
+// ex) 인증 성공하면 resolve, 실패하면 reject
+
+const wait1second = new Promise((resolve, reject) => {
+    console.log('시작');
+    setTimeout(()=>{
+        resolve(console.log('1초 뒤에 찍습니다'));
+    }, 1000)
+
+});
+
+wait1second.then((result)=> {
+    console.log("프라미스 이행 완료");
+});
+
+//시작 -> 1초 뒤에 찍습니다 -> 프라미스 시행 완료
