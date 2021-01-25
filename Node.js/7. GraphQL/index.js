b8f559f529eeb184f2f86225f703aa62dbe6a8a8
pseeej <jinny8517@naver.com>
2021-01-25 17:34:13 +0900

    데이터 수정, 삭제
    const schema = buildSchema(`~~`)에서 어떤 query를 작성하겠다~ 미리 알려주고 하단에 const root = {~}에서 그 함수?의 내용을 적어둠

--- Node.js/7. GraphQL/index.js
+++ Node.js/7. GraphQL/index.js
@@ -24,8 +24,10 @@ const schema = buildSchema(`
 
     type Mutation {
         addProduct( input : ProductInput ) : Product
+        updateProduct( id : ID!, input : ProductInput! ) : Product
+        deleteProduct( id : ID! ) : String
     }
-`); //type Mutation. 입력받은 데이터를 Product로 return
+`); //type Mutation. 입력받은 데이터를 Product로 return. 여기서 어떤 query를 작성하겠다~ 하고 밑에 가서 상세하게 써둠
 
 const products = [{ //임시 데이터 만들어줌
     id : 1,
@@ -47,6 +49,19 @@ const root = {
         products.push(input);   //넣어주는 data push
         return root.getProduct({id : input.id});
     },
+    updateProduct : ({ id, input }) => {    //value에 있는 값과 일치하는 조회 조건을 찾아야함. 필수 요소에 id도 추가됨
+        const index = products.findIndex(product => product.id === parseInt(id)) //배열의 indexing 안에 value로 조회해야함.
+        products[index] = {
+            id : parseInt(id),
+            ...input    //안에서 입력했던 값들 객체 그대로 펼쳐진다고 생각하면 됨. operating 연산자
+        }
+        return products[index];
+    },
+    deleteProduct : ({id}) => {
+        const index = products.findIndex(product => product.id === parseInt(id))
+        products.splice(index, 1)    //products에서 slice로 0번부터 1개의 data가 날아간다고 생각하쇼
+        return "remove success"
+    }
 };
 
 const app = express();
@@ -60,4 +75,3 @@ app.listen(4000, ()=>{  //여기서 4000은 portnum
     console.log('running server port 4000');
 });
 // localhost:4000/graphql 해서 나오는 화면에서 url로 호출하면 응답해주는 graphql 서버 만들어줌
-// 새로 깔은 postman 프로그램에서 get 자리에 http://localhost:4000/graphql?query={nodejs} 입력해주면 사전에 넣어놨던 값 20이 나옴
