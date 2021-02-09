5addd4dc7e09b34a6ece17e273a5c4cbb1413adf
pseeej <jinny8517@naver.com>
2021-02-09 23:16:19 +0900

    1. 플라스크 기본 app.py modified
    app.py 파일 합쳐버림. 기존의 app_jinja2.py가 그냥 app.py로 바뀜

--- Python/6. flask_basic/app.py
+++ Python/6. flask_basic/app.py
@@ -1,29 +1,21 @@
-# venv 설치 : pip3 install virtualenv -> virtualenv flask_fastcampus
-# Flask run으로 실행
-
-# flask alchemy 설치 : pip3 install flask-sqlalchemy
-
 import os
 from flask import Flask
+from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
-basedir = os.path.abspath(os.path.dirname(__file__))    # 현재 있는 file의 절대경로 나타남
-dbfile = os.path.join(basedir, 'db.sqlite') # db 파일 만들기
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile    # sqlalchemy에 필요한 설정 값 넣어주기. sqlite는 파일만 있으면 됨
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  # 사용자 요청이 끝날 때마다 commit하도록 함
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 수정 사항에 대한 track을 하겠다
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)    # 최상위 class 변수
-
-class Test(db.Model):   # M-V-C에서 data 다루는 model
-    __tablename__ = 'test_table'    # db에 들어갈 table명 지정
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(32), unique=True)
-
-db.create_all() # 모델 class 만들어져야 db에 등록 가능
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('hello.html')    # template이라는 디렉토리 안에서 hello.html을 찾아서 실행하라
+
+if __name__ == '__main__':  # 이 파일이 main일 때
+    app.run()   # 실행해라
