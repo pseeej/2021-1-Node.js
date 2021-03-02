5204eb127e5821a4155c216644e06335c3adeb59
pseeej <jinny8517@naver.com>
2021-03-02 23:48:54 +0900

    3-15 검색, 정렬 기능 추가하기
    join, outer join 사용. 아직 잘 모르니깐 나중에 볼 때 다시 한 번 꼭 보길 바람
    https://wikidocs.net/81067

--- Flask/myproject/pybo/views/question_views.py
+++ Flask/myproject/pybo/views/question_views.py
@@ -7,7 +7,9 @@ from werkzeug.utils import redirect
 
 from .. import db
 
-from ..models import Question
+from sqlalchemy import func
+
+from ..models import Question, Answer, User, question_voter
 from ..forms import QuestionForm, AnswerForm
 from pybo.views.auth_views import login_required
 
@@ -20,13 +22,48 @@ bp = Blueprint('question',  __name__, url_prefix='/question')
 # 그냥 list는 python의 예약어라서 사용 불가하므로.
 # 질문 목록 받아오기. order_by로 작성일자 기준 역순으로 정렬
 def _list():
+    # 입력 파라미터
     # localhost:5000/question/list/?page=5에서 5 가져오려고 할 때 사용하는 바로 아래 함수 request.args.get
     page = request.args.get('page', type=int, default=1)    # 페이징 기능 구현
-    question_list = Question.query.order_by(Question.create_date.desc())
+    kw = request.args.get('kw', type=str, default='')
+    so = request.args.get('so', type=str, default='recent')
+
+    # 정렬
+    if so == 'recommend':
+        sub_query = db.session.query(question_voter.c.question_id, func.count('*').label('num_voter')) \
+            .group_by(question_voter.c.question_id).subquery()
+        question_list = Question.query \
+            .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
+            .order_by(sub_query.c.num_voter.desc(), Question.create_date.desc())
+    elif so == 'popular':
+        sub_query = db.session.query(Answer.question_id, func.count('*').label('num_answer')) \
+            .group_by(Answer.question_id).subquery()
+        question_list = Question.query \
+            .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
+            .order_by(sub_query.c.num_answer.desc(), Question.create_date.desc())
+    else:  # recent
+        question_list = Question.query.order_by(Question.create_date.desc())
+    
+    # 조회
+    if kw:
+        search = '%%{}%%'.format(kw)
+        sub_query = db.session.query(Answer.question_id, Answer.content, User.username)\
+            .join(User, Answer.user_id == User.id).subquery()
+        question_list = question_list.join(User) \
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id)\
+                .filter(Question.subject.ilike(search) |    # 질문 제목
+                        Question.content.ilike(search) |    # 질문 내용
+                        User.username.ilike(search) |   # 질문 작성자
+                        sub_query.c.content.ilike(search)   |   # 답변 내용
+                        sub_query.c.username.ilike(search) # 답변 작성자
+                )\
+                .distinct()
+
+    # 페이징
     # paginate 함수로 페이징 적용. paginate(현재 조회할 페이지의 번호, 페이지마다 보여 줄 게시물의 개수)
     # 해당 함수의 반환값은 Pagination 객체. 페이징 처리를 쉽게 만들어줌
     question_list = question_list.paginate(page, per_page=10)
-    return render_template('question/question_list.html', question_list=question_list)
+    return render_template('question/question_list.html', question_list=question_list, page=page, kw=kw, so=so)
 
 
 # question 모델 데이터 중 id값이 question_id인 데이터 조회
